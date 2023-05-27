09392707c2fb4e1fe754c434987da82ce1266812
4zaran <karol.chanaj3@gmail.com>
2023-05-27 16:57:15 +0200

    added: CellularAutomaton - make it rain

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
+
+-- fuzzy search
 vim.keymap.set("n", "<leader>sf", "<cmd>lua fuzzyFindFiles{}<cr>", { desc = "Fuzzy" })
+
+-- CellularAutomaton
+vim.keymap.set("n", "<leader>cc", "<cmd>CellularAutomaton make_it_rain<CR>")
