cd9094335f00747591eac3ee5e2b9d25507f83bb
4zaran <karol.chanaj3@gmail.com>
2023-05-27 15:40:39 +0200

    added: fuzzy search

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- fuzzy search
+local telescope, builtin = require("telescope"), require("telescope.builtin")
+telescope.setup({})
+telescope.load_extension("fzf") -- without this there's no 'foo | ^bar | baz$' style filtering
+function fuzzyFindFiles()
+  builtin.grep_string({
+    path_display = { "smart" },
+    only_sort_text = true,
+    word_match = "-w",
+    search = "",
+  })
+end
