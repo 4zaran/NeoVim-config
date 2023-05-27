21e2ea2c6d5ce767cc798aab92a19630ca38c334
4zaran <karol.chanaj3@gmail.com>
2023-05-27 16:15:24 +0200

    added: syntax highlighting

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "pug",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vue",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
