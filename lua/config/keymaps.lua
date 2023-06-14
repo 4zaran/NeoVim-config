6a92e40b19d2bf5345041f3f28c40aa66217e9a4
Karol Chanaj <karol.chanaj@parleto.io>
2023-06-14 12:26:37 +0200

    changed: diff rc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,13 +7,16 @@
 vim.keymap.set("n", "<leader>cc", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Make it rain" })
 
 -- diffview
-vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Diff current index" })
-vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewOpen rc<cr>", { desc = "Diff rc" })
+-- vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Diff current index" })
+-- vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewOpen rc<cr>", { desc = "Diff rc" })
 vim.keymap.set("n", "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", { desc = "File history" })
 vim.keymap.set("n", "<leader>gx", "<cmd>DiffviewClose <cr>", { desc = "Close Diffview" })
 
 -- fuzzy search
 vim.keymap.set("n", "<leader>sf", "<cmd>lua fuzzyFindFiles{}<cr>", { desc = "Fuzzy" })
+
+-- gitsigns
+vim.keymap.set("n", "<leader>ghg", "<cmd>Gitsigns diffthis rc<cr>", { desc = "Diff rc" })
 
 -- neo-tree
 -- vim.keymap.set("n", "<leader>e", "<cmd>Neotree<cr>", { desc = "Neotree <cwd>" })
